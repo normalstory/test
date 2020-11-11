caa5a7c20d2670c50b326f1f9e008c44c3b243ab
thinknormal <nornalstory@gmail.com>
2020-11-11 16:43:59 +0900

    개정판, Hooks Basic

--- src/components/HooksBasic.js
+++ src/components/HooksBasic.js
@@ -7,6 +7,21 @@ const HooksBasic = () => {
 
   const [color, setColor] = useState('black');
 
+  const object = { a: 1, b: 2, c: 3 };
+  const nextObj = { ...object, d: 4 };
+  console.log('nextObj', nextObj);
+  const array = [
+    { id: 1, value: true },
+    { id: 2, value: true },
+    { id: 3, value: false },
+  ];
+  let nextArray = array.concat({ id: 4 });
+  console.log('arr1:', nextArray);
+  let n2 = nextArray.filter((item) => item.id !== 2);
+  console.log('arr2:', n2);
+  let n3 = nextArray.map((item) => (item.id === 1 ? { ...item, value: false } : item));
+  console.log('arr3:', n3);
+
   return (
     <div>
       <h1>useState</h1>
