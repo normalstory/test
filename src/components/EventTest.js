264e68d0b112657c5375783e07fa87bb01d2c019
thinknormal <nornalstory@gmail.com>
2020-11-11 20:46:06 +0900

    여기까지 클래서에서의 이벤트

--- src/components/EventTest.js
+++ src/components/EventTest.js
@@ -2,27 +2,49 @@ import React, { Component } from 'react';
 
 class EventTest extends Component {
   state = {
+    username: '',
     message: '',
   };
-  constructor(props) {
-    super(props);
-    this.handleChage = this.handleChage.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleChage(e) {
-    //console.log(e.target.value);
-    //const val=e.target.value;
-    //this.setState(e.target.value);
+  //   constructor(props) {
+  //     super(props);
+  //     this.handleChage = this.handleChage.bind(this);
+  //     this.handleClick = this.handleClick.bind(this);
+  //   }
+  //   handleChage(e) {
+  //     //console.log(e.target.value);
+  //     //const val=e.target.value;
+  //     //this.setState(e.target.value);
+  //     this.setState({
+  //       message: e.target.value,
+  //     });
+  //   }
+  handleChage = (e) => {
     this.setState({
-      message: e.target.value,
+      //message: e.target.value,
+      [e.target.name]: e.target.value,
     });
-  }
-  handleClick() {
-    alert(this.state.message);
+  };
+  //   handleClick() {
+  //     alert(this.state.message);
+  //     this.setState({
+  //       message: '',
+  //     });
+  //   }
+  handleClick = () => {
+    alert(this.state.message + '&' + this.state.username);
     this.setState({
+      username: '',
       message: '',
     });
-  }
+  };
+
+  handleKeyPress = (e) => {
+    // if (e.key === 'Enter') {
+    //   this.handleClick();
+    // }
+    e.key === 'Enter' && this.handleClick();
+  };
+
   render() {
     return (
       <div>
@@ -33,6 +55,14 @@ class EventTest extends Component {
           placeholder="내용을 입력하세요."
           value={this.state.message}
           onChange={this.handleChage}
+        ></input>
+        <input
+          type="text"
+          name="username"
+          placeholder="write your name.."
+          value={this.state.username}
+          onChange={this.handleChage}
+          onKeyPress={this.handleKeyPress}
         ></input>
         <button onClick={this.handleClick}>확인</button>
       </div>
